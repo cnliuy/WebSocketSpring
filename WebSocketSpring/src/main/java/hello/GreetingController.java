b17126bb4a7ac10cb77423fc1f06cd465252fbc9
cnliuy <cnliuy@163.com>
2016-09-14 16:33:56 +0800

    0

--- WebSocketSpring/src/main/java/hello/GreetingController.java
+++ WebSocketSpring/src/main/java/hello/GreetingController.java
@@ -1,4 +1,4 @@
-package web;
+package hello;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -6,14 +6,15 @@ import org.springframework.stereotype.Controller;
 import pojo.Greeting;
 import pojo.HelloMessage;
 
+
 @Controller
 public class GreetingController {
 	
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
     public Greeting greeting(HelloMessage message) throws Exception {
-        //Thread.sleep(3000); // simulated delay
-        System.out.println("in  GreetingController()--- greeting method, message:"+message.getName());
+        //Thread.sleep(3000); //simulated delay
+        System.out.println("in  GreetingController() --- greeting method, message:"+message.getName());
         return new Greeting("Hello, " + message.getName() + "!"+message.getAge());
     }
 }
